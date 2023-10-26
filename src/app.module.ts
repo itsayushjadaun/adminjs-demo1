f9914a37cd282be33e0698bafdcd00852312d7a9
itsayushjadaun <jadaunayush3@gmail.com>
2023-10-26 09:52:17 +0530

    add plugin with adminjs

---
+++ src/app.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+const DEFAULT_ADMIN = {
+  email: 'admin@example.com',
+  password: 'password',
+}
+
+const authenticate = async (email: string, password: string) => {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password) {
+    return Promise.resolve(DEFAULT_ADMIN)
+  }
+  return null
+}
+
+@Module({
+  imports: [
+    // AdminJS version 7 is ESM-only. In order to import it, you have to use dynamic imports.
+    import('@adminjs/nestjs').then(({ AdminModule }) => AdminModule.createAdminAsync({
+      useFactory: () => ({
+        adminJsOptions: {
+          rootPath: '/admin',
+          resources: [],
+        },
+        auth: {
+          authenticate,
+          cookieName: 'adminjs',
+          cookiePassword: 'secret'
+        },
+        sessionOptions: {
+          resave: true,
+          saveUninitialized: true,
+          secret: 'secret'
+        },
+      }),
+    })),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
